32fbbbf8930d4d5bf3978d930cbb7a796d18da66
Azri Farid Shahroom <azrifared8482@gmail.com>
2021-12-04 22:33:47 +0800

    fix config

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,15 @@
-const { DB_URL, isProduction } = require('./src/config');
-
+const isProduction = process.env['NODE_ENV'] === 'production';
+const DB_URL = env.get('DB_URL').asString();
 const entitiesPath = isProduction
   ? 'build/entities/*.js'
   : 'src/entities/*.{js,ts}';
+
+if (isProduction) {
+  if (!DB_URL) {
+    console.error('Failed to start server. Missing env JWT_SECRET');
+    process.exit(1);
+  }
+}
 
 module.exports = {
   type: 'mongodb',

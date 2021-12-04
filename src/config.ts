57e71c59dc78372e425d7137528c99cc584010ce
Azri Farid Shahroom <azrifared8482@gmail.com>
2021-12-04 22:30:58 +0800

    fix prod mode

--- src/config.ts
+++ src/config.ts
@@ -7,15 +7,9 @@ if (isProduction) {
     console.error('Failed to start server. Missing env JWT_SECRET');
     process.exit(1);
   }
-
-  if (!process.env['DB_URL']) {
-    console.error('Failed to start server. Missing env DB_URL');
-    process.exit(1);
-  }
 }
 
 export const PORT = env.get('PORT').default(3000).asPortNumber();
 
 export const JWT_SECRET = env.get('JWT_SECRET').asString();
 
-export const DB_URL = env.get('DB_URL').asString();
